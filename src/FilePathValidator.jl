50af121dec4a86854662b652c937ccc49262a428
Arindam Saha <arindamsaha1507@gmail.com>
2025-02-06 13:21:20 +0000

    Added extension field to Filepath

--- src/FilePathValidator.jl
+++ src/FilePathValidator.jl
@@ -1,19 +1,60 @@
+"""
+
+    FilePathValidator
+
+    A structure for file paths.
+
+    # Fields
+    - path::String: The full path.
+    - directory::String: The directory.
+    - file::String: The file name.
+    - extension::String: The file extension.
+"""
 struct FilePath
 
     path::String
     directory::String
     file::String
+    extension::String
 
     function FilePath(path::String)
 
         directory = dirname(path)
         file = basename(path)
-        new(path, directory, file)
+        extension = splitext(file)[2]
+        new(path, directory, file, extension)
 
     end
 
 end
 
+"""
+    Creates a file path from a template string and a dictionary of substitutions.
+
+    # Arguments
+    - `basepath::String`: The template string.
+    - `substitution_dict::Dict{String, Any}`: The dictionary of substitutions.
+
+    # Returns
+    - `FilePath`: The file path object.
+
+    # Examples
+    ```julia
+    substitutions = Dict(
+        "name" => "data",
+        "format" => "csv",
+        "directory" => "dir",
+        "suffix" => 1,
+    )
+
+    template = "{directory}/{name}{suffix}.{format}"
+    filepath = create_filepath_from_template(template, substitutions)
+    println(filepath.path) # "dir/data1.csv"
+    println(filepath.directory) # "dir"
+    println(filepath.file) # "data1.csv"
+    println(filepath.extension) # ".csv"
+    ```
+"""
 function create_filepath_from_template(basepath::String, substitution_dict::Dict{String, Any})
 
     for (key, value) in substitution_dict
--- test/test_filepath_creation.jl
+++ test/test_filepath_creation.jl
@@ -12,13 +12,18 @@ using Supergrassi
     )
 
     template = "{directory}/{name}{suffix}.{format}"
+
+    expected_path = "dir/data1.csv"
     expected_filename = "data1.csv"
     expected_directory = "dir"
+    expected_extension = ".csv"
 
     created_filepath = create_filepath_from_template(template, substitutions)
 
+    @test created_filepath.path == expected_path
     @test created_filepath.file == expected_filename
     @test created_filepath.directory == expected_directory
+    @test created_filepath.extension == expected_extension
 
 
 end
