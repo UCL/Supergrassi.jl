2d98e1ba5802428de15db1b6a4af5d87734430a1
Tuomas Koskela <t.koskela@ucl.ac.uk>
2025-02-07 16:29:40 +0000

    Simplify function to make gradient calculation easier

--- src/compute_demand.jl
+++ src/compute_demand.jl
@@ -1,46 +1,32 @@
-function compute_demand(n::Int,
-                        elasticity::@NamedTuple{_::T, a::T},
-                        log_price_uk::Vector{T},
-                        log_price_eu::Vector{T},
-                        log_price_world::Vector{T},
-                        goods_consumption::@NamedTuple{composite::Vector{T},
-                                                       uk::Vector{T},
-                                                       eu::Vector{T},
-                                                       world::Vector{T}}) where {T <: Real}
-
+# function compute_demand(n,elasticity,log_price_uk,log_price_eu,log_price_world,goods_consumption)
+function compute_demand(elasticity::T,
+                        log_price_uk::T,
+                        log_price_eu::T,
+                        log_price_world::T,
+                        goods_consumption_uk::T,
+                        goods_consumption_eu::T,
+                        goods_consumption_world::T) where {T <: Real}
 
     # Inputs
-    # elasticity.{a} # parms.epsilon
+    # elasticity # parms.epsilon_a
     # log_price_{uk, eu, w} # logPd, parms.logP{eu,w}
-    # goods_consumption.{uk, eu, w} #data.fValue{UK, EU, W}
+    # goods_consumption_{uk, eu, w} #data.fValue{UK, EU, W}
 
     # Outputs
     # alpha.{uk, eu, w}
-    # log_alpha.{uk, eu, w}
-    # ∂_log_alpha.{uk, eu, w}
+    # # log_alpha.{uk, eu, w}
+    # # ∂_log_alpha.{uk, eu, w}
     
-    alpha = Dict("composite" => zeros(n), "uk" => zeros(n), "eu" => zeros(n), "world" => zeros(n))
-    #log_alpha = Dict(composite => ones(n,1), uk => ones(n,1), eu => ones(n,1), world => ones(n,1))
-    #∂_log_alpha = Dict(composite => zeros(n,1), uk => zeros(n,1), eu => zeros(n,1), world => zeros(n,1))
-
-    log_consumer_price_index = zeros(n)
-    composite_price_index = zeros(n)
-    
-    @. log_consumer_price_index = elasticity.a / (elasticity.a - 1.0) * log(
-        goods_consumption.uk ^ -elasticity.a * exp((elasticity.a - 1.0) * log_price_uk / elasticity.a) +
-        goods_consumption.eu ^ -elasticity.a * exp((elasticity.a - 1.0) * log_price_eu / elasticity.a) +
-        goods_consumption.world  ^ -elasticity.a * exp((elasticity.a - 1.0) * log_price_world  / elasticity.a)
+    log_consumer_price_index = elasticity / (elasticity - 1.0) * log(
+        goods_consumption_uk ^ -elasticity * exp((elasticity - 1.0) * log_price_uk / elasticity) +
+        goods_consumption_eu ^ -elasticity * exp((elasticity - 1.0) * log_price_eu / elasticity) +
+        goods_consumption_world  ^ -elasticity * exp((elasticity - 1.0) * log_price_world  / elasticity)
     ) #logPf
 
-    @. composite_price_index = goods_consumption.composite ^ -elasticity._ * exp((elasticity._ - 1.0) * log_consumer_price_index / elasticity._)
-    
-    @. alpha["composite"] = goods_consumption.composite * exp( (elasticity.a - 1.0) *
-        (log_consumer_price_index - elasticity._/(elasticity._ - 1.0) * log( sum( composite_price_index) ) ) )
+    alpha_uk = goods_consumption_uk * exp((elasticity - 1.0) * (log_price_uk - log_consumer_price_index))
+    alpha_eu = goods_consumption_eu * exp((elasticity - 1.0) * (log_price_eu - log_consumer_price_index))
+    alpha_world = goods_consumption_world * exp((elasticity - 1.0) * (log_price_world - log_consumer_price_index))
 
-    @. alpha["uk"] = goods_consumption.uk * exp((elasticity.a - 1.0) * (log_price_uk - log_consumer_price_index))
-    @. alpha["eu"] = goods_consumption.uk * exp((elasticity.a - 1.0) * (log_price_eu - log_consumer_price_index))
-    @. alpha["world"] = goods_consumption.uk * exp((elasticity.a - 1.0) * (log_price_world - log_consumer_price_index))
-
-    return alpha
+    return alpha_uk, alpha_eu, alpha_world
     
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -8,26 +8,36 @@ end
 @testset "Compute demand" begin
 
     n = 16
-    elasticity = (_ = 3.0, a = 2.0)
+    elasticity = 2.0
     log_price = (uk = ones(n), eu = ones(n), world = ones(n))
-    goods_consumption = (composite = ones(n), uk = ones(n), eu = ones(n), world = ones(n))
+    goods_consumption = (uk = ones(n), eu = ones(n), world = ones(n))
 
-    alpha = Supergrassi.compute_demand(n,
-                                       elasticity,
-                                       log_price.uk,
-                                       log_price.eu,
-                                       log_price.world,
-                                       goods_consumption)
+    alpha = Supergrassi.compute_demand.(elasticity,
+                                        log_price.uk,
+                                        log_price.eu,
+                                        log_price.world,
+                                        goods_consumption.uk,
+                                        goods_consumption.eu,
+                                        goods_consumption.world)
 
-    @test haskey(alpha, "composite")
-    @test haskey(alpha, "uk")
-    @test haskey(alpha, "eu")
-    @test haskey(alpha, "world")
-
-    for key in keys(alpha)
-        @test length(alpha[key]) == n
-        @test isfinite(sum(alpha[key]))
+    @test length(alpha) == n
+    for industry in alpha
+        @test isfinite(sum(industry))
     end
+
+    backend = AutoZygote()
+    pd = [ones(n), 2*ones(n), 3*ones(n)]
+
+    f(el,peu,pw,cd,ceu,cworld) = pd -> Supergrassi.compute_demand(el,pd,peu,pw,cd,ceu,cworld)
+
+    g = gradient.(f.(elasticity,
+                     log_price.eu,
+                     log_price.world,
+                     goods_consumption.uk,
+                     goods_consumption.eu,
+                     goods_consumption.world),
+                  backend,
+                  pd)
     
 end
 
