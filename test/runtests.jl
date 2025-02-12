6fcf756304e40de1ebd901b4d8ca77acc66f6de6
Tuomas Koskela <t.koskela@ucl.ac.uk>
2025-02-12 17:07:27 +0000

    Add test with gradient of scalar function using Enzyme

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using Supergrassi
 using Test
+using Enzyme
 
 @testset "Supergrassi.jl" begin
     # Write your tests here.
@@ -9,7 +10,7 @@ end
 
     n = 16
     elasticity = 2.0
-    log_price = (uk = ones(n), eu = ones(n), world = ones(n))
+    log_price = (uk = randn(n), eu = randn(n), world = randn(n))
     goods_consumption = (uk = ones(n), eu = ones(n), world = ones(n))
 
     alpha = Supergrassi.compute_demand.(elasticity,
@@ -21,33 +22,43 @@ end
                                         goods_consumption.world)
 
     @test length(alpha) == n
-    for industry in alpha
-        @test isfinite(sum(industry))
-    end
-
-    backend = AutoZygote()
-    pd = [ones(n), 2*ones(n), 3*ones(n)]
-
-    f(el,peu,pw,cd,ceu,cworld) = pd -> Supergrassi.compute_demand(el,pd,peu,pw,cd,ceu,cworld)
-
-    g = gradient.(f.(elasticity,
-                     log_price.eu,
-                     log_price.world,
-                     goods_consumption.uk,
-                     goods_consumption.eu,
-                     goods_consumption.world),
-                  backend,
-                  pd)
+    @test all(i -> minimum(i) > 0, alpha)
     
+    f(el, pd, peu, pw, fd, feu, fw) = gradient(Forward,
+                                               Supergrassi.compute_demand,
+                                               Const(el),
+                                               pd,
+                                               Const(peu),
+                                               Const(pw),
+                                               Const(fd),
+                                               Const(feu),
+                                               Const(fw))
+    
+    grad_alpha = f.(elasticity,
+                    log_price.uk,
+                    log_price.eu,
+                    log_price.world,
+                    goods_consumption.uk,
+                    goods_consumption.eu,
+                    goods_consumption.world)
+
+    # Test grad_alpha is of right shape
+    @test length(grad_alpha) == n
+    @test all(i -> length(i) == 7, grad_alpha)
+    # Test gradients of Consts are `nothing`
+    @test all(i -> isnothing(i[1]), grad_alpha)
+    @test all(i -> all(j -> isnothing(j), i[3:7]), grad_alpha)
+    # Test gradients of `pd` are finite
+    @test all(i -> all(j -> isfinite(j), i[2]), grad_alpha)
 end
 
-@testset "Compute equilibrium test" begin
+# @testset "Compute equilibrium test" begin
 
-    using Distributions
-    using Random
+#     using Distributions
+#     using Random
     
-    n = 16
-    log_consumer_price_index = zeros(n)
-    rand!(Normal(), log_consumer_price_index)
+#     n = 16
+#     log_consumer_price_index = zeros(n)
+#     rand!(Normal(), log_consumer_price_index)
     
-end
+# end
