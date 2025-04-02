619ab8aad23955d730bb3908ab0058365ee9554d
Arindam Saha <arindamsaha1507@gmail.com>
2025-04-02 11:39:21 +0100

    Cleanup till line 66

--- src/Cleanup.jl
+++ src/Cleanup.jl
@@ -1,11 +1,12 @@
 using DataFrames
+using Statistics
 
 function select_year(data::DataFrame, year::Int64)
     return data[data.year .== year, 4:end]
 end
 
-function sum_dataframe_row_wise(data::DataFrame)
-    return combine(data, names(data, Real) .=> sum)
+function combine_dataframe_row_wise(data::DataFrame, func::Function)
+    return combine(data, names(data, Real) .=> func, renamecols=false)
 end
 
 
@@ -17,11 +18,14 @@ struct CleanData
     low_income_share::DataFrame
     high_income_share::DataFrame
 
-    function CleanData(low_income::DataFrame, high_income::DataFrame)
+    mean_capital_current_year::DataFrame
+    mean_capital_next_year::DataFrame
+
+    function CleanData(low_income::DataFrame, high_income::DataFrame, mean_capital_current_year::DataFrame, mean_capital_next_year::DataFrame)
         high_income_share = high_income ./ (high_income .+ low_income)
         low_income_share = low_income ./ (high_income .+ low_income)
 
-        return new(low_income, high_income, low_income_share, high_income_share)
+        return new(low_income, high_income, low_income_share, high_income_share, mean_capital_current_year, mean_capital_next_year)
         
     end
 
@@ -29,10 +33,16 @@ end
 
 function cleanup(data::Data, year::Int64)
     low_income = select_year(data.household.income.low, year)
-    low_income = sum_dataframe_row_wise(low_income)
+    low_income = combine_dataframe_row_wise(low_income, sum)
 
     high_income = select_year(data.household.income.high, year)
-    high_income = sum_dataframe_row_wise(high_income)
+    high_income = combine_dataframe_row_wise(high_income, sum)
 
-    return CleanData(low_income, high_income)
+    capital_current_year = select_year(data.industry.capital, year)
+    capital_current_year = combine_dataframe_row_wise(capital_current_year, mean)
+
+    capital_next_year = select_year(data.industry.capital, year + 1)
+    capital_next_year = combine_dataframe_row_wise(capital_next_year, mean)
+
+    return CleanData(low_income, high_income, capital_current_year, capital_next_year)
 end
