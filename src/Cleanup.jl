f47237db2e9956b04e1040f0f770815ff4137bed
Arindam Saha <arindamsaha1507@gmail.com>
2025-04-04 15:05:00 +0100

    Further cleanup of 'other'

--- src/Cleanup.jl
+++ src/Cleanup.jl
@@ -9,6 +9,12 @@ function combine_dataframe_row_wise(data::DataFrame, func::Function)
     return combine(data, names(data, Real) .=> func, renamecols=false)
 end
 
+function get_rows(data::DataFrame, row_names::String, col_names::Array{String, 1})
+    rr = data[data.x2 .== row_names, 4:end]
+    rename!(rr, col_names)
+    return rr
+end
+
 
 struct CleanData
 
@@ -21,11 +27,23 @@ struct CleanData
     mean_capital_current_year::DataFrame
     mean_capital_next_year::DataFrame
 
-    function CleanData(low_income::DataFrame, high_income::DataFrame, mean_capital_current_year::DataFrame, mean_capital_next_year::DataFrame)
+    tax_products::DataFrame
+    tax_production::DataFrame
+
+    compensation_employees::DataFrame
+    gross_operating_surplus_and_mixed_income::DataFrame
+
+    function CleanData(low_income::DataFrame, high_income::DataFrame, mean_capital_current_year::DataFrame, mean_capital_next_year::DataFrame, others::DataFrame, industry_names::Array{String, 1})
         high_income_share = high_income ./ (high_income .+ low_income)
         low_income_share = low_income ./ (high_income .+ low_income)
 
-        return new(low_income, high_income, low_income_share, high_income_share, mean_capital_current_year, mean_capital_next_year)
+        tax_products = get_rows(others, "Taxes less subsidies on products", industry_names)
+        tax_production = get_rows(others, "Taxes less subsidies on production", industry_names)
+
+        compensation_employees = get_rows(others, "Compensation of employees", industry_names)
+        gross_operating_surplus_and_mixed_income = get_rows(others, "Gross operating surplus and mixed income", industry_names)
+
+        return new(low_income, high_income, low_income_share, high_income_share, mean_capital_current_year, mean_capital_next_year, tax_products, tax_production, compensation_employees, gross_operating_surplus_and_mixed_income)
         
     end
 
@@ -44,5 +62,7 @@ function cleanup(data::Data, year::Int64)
     capital_next_year = select_year(data.industry.capital, year + 1)
     capital_next_year = combine_dataframe_row_wise(capital_next_year, mean)
 
-    return CleanData(low_income, high_income, capital_current_year, capital_next_year)
+    industry_names = data.input_output.industry_names
+
+    return CleanData(low_income, high_income, capital_current_year, capital_next_year, data.others, industry_names)
 end
