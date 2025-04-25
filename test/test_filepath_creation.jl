8dd38870a7bc06667b4c63c6fef5bb59eb6f2d0f
Tuomas Koskela <t.koskela@ucl.ac.uk>
2025-04-25 15:11:32 +0100

    Fix broken test

--- test/test_filepath_creation.jl
+++ test/test_filepath_creation.jl
@@ -3,7 +3,6 @@ using Supergrassi
 
 @testset "Filename Templating" begin
 
-
     substitutions = Dict(
         "name" => "data",
         "format" => "csv",
@@ -18,12 +17,11 @@ using Supergrassi
     expected_directory = "dir"
     expected_extension = ".csv"
 
-    created_filepath = create_filepath_from_template(template, substitutions)
+    created_filepath = Supergrassi.create_filepath_from_template(template, substitutions)
 
     @test created_filepath.path == expected_path
     @test created_filepath.file == expected_filename
     @test created_filepath.directory == expected_directory
     @test created_filepath.extension == expected_extension
-
 
 end
