c2dda4ea86da70f9bb93743b0161cb540ef4ee87
Tuomas Koskela <t.koskela@ucl.ac.uk>
2025-05-07 16:06:46 +0100

    Fix typo, revert comprehensions

--- src/utility_function_parameters.jl
+++ src/utility_function_parameters.jl
@@ -77,7 +77,14 @@ function total_parameters(log_price_index::Vector{T}, quantity::Vector{T}, elast
     length(log_price_index) == length(quantity) || error()
 
     logPBar = log_total_price_index(elasticity, log_price_index, quantity)
-    return [parameters[i] = weight_kernel(quantity[i], exp(log_price_index[i] - logPBar), elasticity) for i in eachindex(quantity, log_price_index)]
+
+    parameters = Vector{T}(undef, length(log_price_index))
+    for i in axes(log_price_index,1)
+        parameters[i] = weight_kernel(quantity[i], exp(log_price_index[i] - logPBar), elasticity)
+    end
+    return parameters
+
+    #return [parameters[i] = weight_kernel(quantity[i], exp(log_price_index[i] - logPBar), elasticity) for i in eachindex(quantity, log_price_index)]
 
 end
 
@@ -171,7 +178,15 @@ function total_input_parameters(log_price_index::Vector{T}, input::Vector{T},
     length(log_price_index) == length(input) || error()
     tauP = tauPdMu(elasticity, log_price_index, input, capital, demand0, output, labor, log_wages, tau)
 
-    return [weight_kernel(input[i], exp(log_price_index[i]) / tauP, elasticity) for i in eachindex(input, log_price_index)]
+    parameters = Vector{T}(undef, length(log_price_index))
+
+    for i in axes(log_price_index,1)
+        parameters[i] = weight_kernel(input[i], exp(log_price_index[i])/tauP, elasticity)
+    end
+
+    return parameters
+
+    #return [weight_kernel(input[i], exp(log_price_index[i]) / tauP, elasticity) for i in eachindex(input, log_price_index)]
 
 end
 
@@ -218,7 +233,13 @@ end
 
 function sum_kernel(var::Vector{T}, logP::Vector{T}, elasticity::T) where {T <: Real}
 
-    return sum(x -> x[1] ^ (1 / elasticity) * exp((elasticity - 1) * x[2] / elasticity), zip(var, logP))
+    s = 0.0
+    for i in axes(logP,1)
+        s += var[i] ^ (1.0 / elasticity) * exp((elasticity - 1.0) * logP[i] / elasticity)
+    end
+    return s
+
+    #return sum(x -> x[1] ^ (1 / elasticity) * exp((elasticity - 1) * x[2] / elasticity), zip(var, logP))
 
 end
 
