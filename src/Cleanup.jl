5834b1f01f68d95cb84551973cd95753f1bd0892
Arindam Saha <arindamsaha1507@gmail.com>
2025-06-02 11:49:30 +0100

    Removed common struct to be used later

--- src/Cleanup.jl
+++ src/Cleanup.jl
@@ -602,28 +602,6 @@ function generate_constants(data::Data, settings::Dict{String, Any})
 
 end
 
-struct CommonVariables
-    year::Int64
-    industry_names::Vector{String}
-    aggregated_names::Vector{String}
-    mapping_105_to_64::Dict{String, String}
-    mapping_64_to_16::Dict{String, String}
-
-    split::Float64
-    industries_in_cols::Bool
-
-    tax_products::DataFrame
-    tax_production::DataFrame
-    sic64::Vector{String}
-
-
-    exchange_rates::ExchangeRates
-    interest_rate::Float64
-
-    total_imports_from_uk::TotalImports
-    total_imports_from_all_sources::TotalImports
-end
-
 """
     clean_data(data::Data, settings::Dict{String, Any})
 
