b47d4d3886f7a3c1d860c7a629baa886504a8db9
Tuomas Koskela <t.koskela@ucl.ac.uk>
2025-06-02 16:48:40 +0100

    Fix typo

--- src/Cleanup.jl
+++ src/Cleanup.jl
@@ -597,9 +597,9 @@ function generate_constants(data::Data, settings::Dict{String, Any})
     elasticities = settings["constants"]["elasticities"]
 
     production_elasticity = Elasticity(elasticities["production"][1],
-                                        elasticities["production"][3],
-                                        nothing,
-                                        elasticities["production"][2])
+                                       elasticities["production"][3],
+                                       nothing,
+                                       elasticities["production"][2])
 
     world_export_demand_elasticity = Elasticity(elasticities["rest_of_world_export_demand"][1],
                                                 elasticities["rest_of_world_export_demand"][2],
@@ -607,9 +607,9 @@ function generate_constants(data::Data, settings::Dict{String, Any})
                                                 nothing)
 
     eu_export_demand_elasticity = Elasticity(elasticities["eu_export_demand"][1],
-                                                elasticities["eu_export_demand"][2],
-                                                elasticities["eu_export_demand"][3],
-                                                nothing)
+                                             elasticities["eu_export_demand"][2],
+                                             elasticities["eu_export_demand"][3],
+                                             nothing)
 
     consumption_elasticity = Elasticity(elasticities["consumption"][1],
                                         elasticities["consumption"][2],
@@ -617,20 +617,20 @@ function generate_constants(data::Data, settings::Dict{String, Any})
                                         nothing)
 
     investment_elasticity = Elasticity(elasticities["investment"][1],
-                                        elasticities["investment"][2],
-                                        nothing,
-                                        nothing)
+                                       elasticities["investment"][2],
+                                       nothing,
+                                       nothing)
 
-    elasticities_struct = Elasiticities(production_elasticity,
-                                 world_export_demand_elasticity,
-                                 eu_export_demand_elasticity,
-                                 consumption_elasticity,
-                                 investment_elasticity)
+    elasticities_struct = Elasticities(production_elasticity,
+                                       world_export_demand_elasticity,
+                                       eu_export_demand_elasticity,
+                                       consumption_elasticity,
+                                       investment_elasticity)
 
     interest_rates = data.risk_free_rate[Dates.year.(data.risk_free_rate.date) .== year, 2:end]
     parse_string_dataframe!(interest_rates, Float64)
     interest_rate = 1 + geomean(interest_rates[!, 1] / 100)
-                                              
+
     return Constants(year, exchange_rates, interest_rate, total_imports_from_uk, total_imports_from_all_sources, elasticities_struct)
 
 end
