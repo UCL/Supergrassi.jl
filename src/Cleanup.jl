12b5dab29e9a012143f7a9f853993d4d08131d9b
Arindam Saha <arindamsaha1507@gmail.com>
2025-06-09 08:32:07 +0100

    Added a basic common variables struct

--- src/Cleanup.jl
+++ src/Cleanup.jl
@@ -635,6 +635,22 @@ function generate_constants(data::Data, settings::Dict{String, Any})
 
 end
 
+struct CommonVariables
+    year::Int64
+    industry_names::Vector{String}
+    aggregated_names::Vector{String}
+    mapping_105_to_64::Dict{String, String}
+    mapping_64_to_16::Dict{String, String}
+
+    split::Float64
+    industries_in_cols::Bool
+
+    tax_products::DataFrame
+    tax_production::DataFrame
+    sic64::Vector{String}
+
+end
+
 """
     clean_data(data::Data, settings::Dict{String, Any})
 
