6b4152dad36d2b4f5ec6e11db6ca14b71903c3e1
Arindam Saha <arindamsaha1507@gmail.com>
2025-07-05 23:35:50 +0100

    Added a main simulate function

--- src/Supergrassi.jl
+++ src/Supergrassi.jl
@@ -13,7 +13,44 @@ include("parameters.jl")
 include("parameters_interface.jl")
 include("excess_demand.jl")
 
+function simulate()
+    @info "Simulation started."
+
+    path = joinpath(@__DIR__, "..", "config","settings.yml")
+    settings_path = create_filepath(path)
+    settings = read_settings(settings_path)
+    filepaths = check_file_availability(settings)
+    data = read_data(filepaths, settings)
+
+    @info "Data read successfully."
+    
+    clean = Supergrassi.clean_data(data,settings)
+    Supergrassi.postprocess_clean_data!(clean)
+
+    @info "Data cleaned and post-processed."
+
+    df = outerjoin(CSV.read(joinpath(@__DIR__,"..","data", "data_for_household_demand.csv"), DataFrame),
+    CSV.read(joinpath(@__DIR__, "..", "data", "data_for_eu_demand.csv"), DataFrame),
+    CSV.read(joinpath(@__DIR__, "..", "data", "data_for_row_demand.csv"), DataFrame),
+    CSV.read(joinpath(@__DIR__, "..", "data", "data_for_capital_production.csv"), DataFrame),
+    on = [:logP_uk, :logP_eu, :logP_w], makeunique = true)
+
+    prices = DataFrame([df.logP_uk, df.logP_eu, df.logP_w], ["uk", "eu", "world"])
+
+    @info "Prices extracted from data."
+
+    params, ∂params = Supergrassi.compute_all_parameters(clean, prices, false)
+    log_params, ∂log_params = Supergrassi.compute_all_parameters(clean, prices, true)
+
+    @info "Parameters computed."
+
+    @info "Simulation completed."
+
+    return settings, data, clean, params, ∂params, log_params, ∂log_params
+end
+
 export create_filepath, read_data, read_settings, check_file_availability
 export clean_data
+export simulate
 
 end
