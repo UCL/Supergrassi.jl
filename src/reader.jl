4e58dbef1f2d43b677eb7189d075f37e7e289617
Arindam Saha <arindamsaha1507@gmail.com>
2025-07-07 14:52:32 +0100

    Added all docstrings

--- src/reader.jl
+++ src/reader.jl
@@ -3,6 +3,24 @@ using DataFrames
 using XLSX
 using Logging
 
+"""
+    read_excel(file::String; sheet::String="Sheet1", range::String="A1:Z1000")
+
+Reads an Excel file and returns a DataFrame.
+
+# Arguments
+- `file::String`: The path to the Excel file.
+- `sheet::String`: The name of the sheet to read. Default is "Sheet1".
+- `range::String`: The range of cells to read, in Excel format (e.g., "A1:Z1000"). Default is "A1:Z1000".
+
+# Returns
+- `DataFrame`: A DataFrame containing the data from the specified sheet and range.
+
+# Examples
+```julia
+df = read_excel("data.xlsx", sheet="Sheet1", range="A1:Z1000")
+```
+"""
 function read_excel(file::String; sheet::String="Sheet1", range::String="A1:Z1000")
 
     range_regex = r"^([A-Z]{1,3}[0-9]{1,7}(:[A-Z]{1,3}[0-9]{1,7})?|[A-Z]{1,3}:[A-Z]{1,3}|[0-9]{1,7}:[0-9]{1,7})$"
@@ -28,6 +46,22 @@ function read_excel(file::String; sheet::String="Sheet1", range::String="A1:Z100
 end
 
 
+"""
+    read_csv(file::String)
+
+Reads a CSV file and returns a DataFrame.
+
+# Arguments
+- `file::String`: The path to the CSV file.
+
+# Returns
+- `DataFrame`: A DataFrame containing the data from the CSV file.
+
+# Examples
+```julia
+df = read_csv("data.csv")
+```
+"""
 function read_csv(file::String)
     return CSV.read(file, DataFrames.DataFrame)
 end
@@ -70,6 +104,9 @@ Reads a data file and returns a DataFrame.
 - `sheet::String`: The name of the sheet.
 - `range::String`: The range of cells to read.
 
+# Returns
+- `DataFrame`: A DataFrame containing the data from the specified sheet and range.
+
 # Examples
 ```julia
 df = read_data("data.xlsx", "Sheet1", "A1:Z1000")
@@ -89,6 +126,26 @@ function read_data(file::String, sheet::String, range::String)
 
 end
 
+"""
+    read_data(filepaths::Dict{String, FilePath}, settings::Dict{String, Any})
+
+Reads multiple data files and returns a `Data` object.
+
+# Arguments
+- `filepaths::Dict{String, FilePath}`: A dictionary where keys are identifiers and values are `FilePath` objects.
+- `settings::Dict{String, Any}`: A dictionary containing settings, including limits for processing the data.
+
+# Returns
+- `Data`: An instance of the `Data` struct containing processed data from the files.
+
+# Examples
+```julia
+data_files = Dict(
+    "input_output" => FilePath("data/input_output.xlsx"),
+    "assets_liabilities" => FilePath("data/assets_liabilities.csv")
+)
+```
+"""
 function read_data(filepaths::Dict{String, FilePath}, settings::Dict{String, Any})
     data = Dict{String, DataFrame}()
 
