1a196f57328d0b55fa70ea4e403e18c909917000
Arindam Saha <arindamsaha1507@gmail.com>
2025-07-10 13:47:00 +0100

    Tested Parameters by Region and

--- test/test_parameters.jl
+++ test/test_parameters.jl
@@ -3,6 +3,12 @@ using Supergrassi, Test
 
 @testset "Unit Tests for Parameters" begin
 
-    @test Supergrassi.parameter_by_region(0, 0, 0, 0) == 0
+    @testset "Parameters by region" begin
+        @test Supergrassi.parameter_by_region(0, 0, 0, 0) == 0
+        @test Supergrassi.parameter_by_region(0, 1, 5, 5) == 1
+
+        @test Supergrassi.log_parameter_by_region(0, 0, 0, 0) == 0
+        @test Supergrassi.log_parameter_by_region(0, 1, 5, 5) == log(Supergrassi.parameter_by_region(0, 1, 5, 5))
+    end
 
 end
