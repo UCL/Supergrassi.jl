bc33b6b06e411fbb439a16b98865aac59c41a12e
Arindam Saha <arindamsaha1507@gmail.com>
2025-07-10 18:56:36 +0100

    Tested the variants of Parameters by Region

--- test/test_parameters.jl
+++ test/test_parameters.jl
@@ -9,6 +9,9 @@ using Supergrassi, Test
 
         @test Supergrassi.log_parameter_by_region(0, 0, 0, 0) == 0
         @test Supergrassi.log_parameter_by_region(0, 1, 5, 5) == log(Supergrassi.parameter_by_region(0, 1, 5, 5))
+
+        @test length(Supergrassi.parameters_by_region(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0)) == 3
+        @test length(Supergrassi.log_parameters_by_region(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0)) == 3
     end
 
 end
