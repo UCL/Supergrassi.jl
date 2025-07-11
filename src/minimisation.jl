a0b1fe16a963e276c10a2754357f3c0c1536366b
Arindam Saha <arindamsaha1507@gmail.com>
2025-07-11 15:56:25 +0100

    Playing with a dummy model

---
+++ src/minimisation.jl
@@ -0,0 +1,22 @@
+using JuMP
+using Ipopt
+
+function minimisation()
+    model = Model(Ipopt.Optimizer)
+
+    @variable(model, x >= 0)
+    @variable(model, y >= 0)
+
+    @objective(model, Min, (x - 1)^2 + (y - 2)^2)
+
+    # @constraint(model, x + y == 3)
+
+    optimize!(model)
+
+    return value(x), value(y)
+
+end
+
+# Example usage
+x_opt, y_opt = minimisation()
+println("Optimal solution: x = $x_opt, y = $y_opt")
