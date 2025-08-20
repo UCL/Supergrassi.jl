3052bf9dbb3157b8ff3de7a269736b810c2b5937
Arindam Saha <arindamsaha1507@gmail.com>
2025-08-20 11:25:49 +0100

    One step back... sort of

--- src/minimisation.jl
+++ src/minimisation.jl
@@ -1,15 +1,17 @@
 using JuMP
 using Ipopt
 
-function minimisation(x::Vector{<:Number}, data::CleanData, params::Parameters)
+# function minimisation(x::Vector{<:Number}, data::CleanData, params::Parameters)
+function minimisation()
     model = Model(Ipopt.Optimizer)
 
     @variable(model, x >= 0)
-    # @variable(model, y >= 0)
+    @variable(model, y >= 0)
 
-    @objective(model, Min, compute_objective_function(x, data, params))
+    # @objective(model, Min, compute_objective_function(x, data, params))
+    @objective(model, Min, x^2 + y^2 - x*y)
 
-    # @constraint(model, x + y == 5)
+    @constraint(model, x + y == 5)
 
     optimize!(model)
 
@@ -18,5 +20,5 @@ function minimisation(x::Vector{<:Number}, data::CleanData, params::Parameters)
 end
 
 # Example usage
-# x_opt, y_opt = minimisation()
-# println("Optimal solution: x = $x_opt, y = $y_opt")
+x_opt, y_opt = minimisation()
+println("Optimal solution: x = $x_opt, y = $y_opt")
