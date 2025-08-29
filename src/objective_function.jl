0b8b898fd7c9cf018a455783df5ae7260785b2bc
Arindam Saha <arindamsaha1507@gmail.com>
2025-08-29 16:53:39 +0100

    Cleaned up

--- src/objective_function.jl
+++ src/objective_function.jl
@@ -15,13 +15,10 @@ Computes the objective function value based on the log prices and zOC values.
 function compute_objective_function(log_price_uk::Vector{<:Number}, zOC::Vector{<:Number}, data::CleanData, params::ParameterSubset)
 
     tau = compute_advalorem_tax(data.industry)
-    # tau = rand(length(log_price_uk))
 
     mu = params.production.shock_mean
-    # mu = rand(length(log_price_uk))
 
     gammaK = params.production.capital
-    # gammaK = rand(length(log_price_uk))
 
     k0 = data.industry.capital.current_year
 
@@ -34,8 +31,6 @@ function compute_objective_function(log_price_uk::Vector{<:Number}, zOC::Vector{
     objective_value = 0.5 * sum(e.^2)
 
     return objective_value
-
-    # return sum(abs.(excess_demand))
 
 end
 
@@ -54,34 +49,13 @@ Computes the objective function value based on a vector of parameters.
 """
 function compute_objective_function(x::Vector{<:Number}, data::CleanData, prices_eu::Vector{<:Number}, prices_world::Vector{<:Number})
 
-    # n = length(data.industry.regional.total_use.agg)
     n = 16
 
     log_price_uk = x[1:n]
     zOC = x[(n+1):end]
 
-    # params = compute_all_parameters(data, log_price_uk, prices_eu, prices_world, false)
-
     param_subset = compute_parameter_subset(data, log_price_uk, prices_eu, prices_world, false)
-
-    println()
-    println("Params ")
-    println(param_subset.constants)
-    println("===========")
-
-    # exit()
 
     return compute_objective_function(log_price_uk, zOC, data, param_subset)
 
 end
-
-
-# function compute_objective_function(x::Vector{<:Number}, data::CleanData, prices_eu::Vector{<:Number}, prices_world::Vector{<:Number})
-
-#     log_price_uk = x[1:n]
-
-#     # params = compute_all_parameters(data, prices_df)
-
-#     return compute_objective_function(x, data, params[1])
-
-# end
