6dd6a18ae5b577fcdb8ce731d87c99224d41b215
Tuomas Koskela <t.koskela@ucl.ac.uk>
2025-09-19 11:39:22 +0100

    stop segfaulting

--- src/Supergrassi.jl
+++ src/Supergrassi.jl
@@ -5,32 +5,25 @@ using Ipopt
 using LinearAlgebra
 using SparseArrays
 
-
-function sparser(matrix)
-    sp = sparse(matrix)
-    return findnz(sp)
-end
-
 function compute_objective_function(x::Vector{<:Number})
     return sum(x)
 end
 
-function compute_gradient(x::Vector{<:Number}, gradient_var::Vector{Float64})
-    gradient_var .= ones(length(x))
-    return gradient_var
-end
-
-function compute_constraint_jacobian(x::Vector{<:Number})
-    y = diagm(ones(length(x[1:32])))
+function compute_gradient(x::Vector{<:Number}, y::Vector{Float64})
+    y[:] = ones(length(x))
     return y
 end
+
+# function compute_constraint_jacobian(x::Vector{<:Number})
+#     #y = diagm(ones(length(x[1:4])))
+#     y = diagm(ones(4))
+#     return y
+# end
 
 function constraint_function(x::Vector{T}, y::Vector{T}) where {T <: Real}
-    y = 0.5 .* x[1:32] .^ 2
+    y = 0.5 .* x[1:4] .^ 2
     return y
 end
-
-
 
 function compute_hessian(x::Vector{Float64},
                   rows::Vector{Int32},
@@ -43,43 +36,45 @@ function compute_hessian(x::Vector{Float64},
     return
 end
 
+function simple_jacobian(x::Vector{Float64}, rows::Vector{Int32}, cols::Vector{Int32}, vals::Nothing)
 
+    if ~isdefined(Supergrassi, :Jg)
+        error("Jacobian must be defined in the scope that calls this function")
+    end
+    
+    sparse_jacobian = findnz(sparse(Jg))
+    rows[:] = sparse_jacobian[1]
+    cols[:] = sparse_jacobian[2]
 
+    return
+    
+end
+
+function simple_jacobian(x, rows, cols, vals::Vector{Float64})
+    
+    sparse_jacobian = findnz(sparse(diagm(ones(length(x[1:4])))))
+    vals[:] = sparse_jacobian[3]
+    
+    return 
+end
+
+Jg = diagm(1 => ones(3), 3 => [1])
 
 function estimate()
 
-    x = rand(50)
+    x = rand(5)
+    #Jg = diagm(ones(length(x[1:4])))
     println("Starting minimisation with x of length ", length(x))
-
-
-    function simple_jacobian(x, rows, cols, vals)
-
-        sparse_jacobian = sparser(compute_constraint_jacobian(x))
-        
-        if vals === nothing
-            rows[:] = sparse_jacobian[1]
-            cols[:] = sparse_jacobian[2]
-        else
-            vals[:] = sparse_jacobian[3]
-        end
-
-        return 
-    end
-
-    function compute_non_zero_elements_in_jacobian(x)
-        trial = sparser(compute_constraint_jacobian(x))
-        return length(trial[1])
-    end
-
-    n_non_zero = compute_non_zero_elements_in_jacobian(x)
+    
+    n_non_zero = 4
 
     prob = Ipopt.CreateIpoptProblem(
-        50,
-        [0.0 for i in 1:50],
-        [10.0 for i in 1:50],
-        32,
-        [0.0 for i in 1:32],
-        [10.0 for i in 1:32],
+        5,
+        [0.0 for i in 1:5],
+        [10.0 for i in 1:5],
+        4,
+        [0.0 for i in 1:4],
+        [10.0 for i in 1:4],
         n_non_zero,
         0,
         compute_objective_function,
