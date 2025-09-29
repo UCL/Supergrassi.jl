e7ddb1ee4d1cb7e5ddc6f16a57075ac9da9ee4bd
Tuomas Koskela <t.koskela@ucl.ac.uk>
2025-09-29 15:01:12 +0100

    Update docstrings

--- src/constraint_function.jl
+++ src/constraint_function.jl
@@ -1,15 +1,17 @@
 using LinearAlgebra
 
 """
-    function compute_constraint_function(x::Vector{<:Number}, data::CleanData, params::Parameters)
+    function constraint_jacobian(x::Vector{<:Number}, data::CleanData, params::Parameters)
 
 # Arguments
 - `x::Vector{<:Number}`: Vector containing equilibrium variables
+- `log_price_eu::Vector{<:Number}`: Vector containing the EU prices
+- `log_price_world::Vector{<:Number}`: Vector containing the rest of the world prices
 - `data::CleanData`: Cleaned data structure containing industry and regional information.
 - `params::Parameters`: Parameters structure containing production and constants.
 """
-function constraint_jacobian(x::Vector{<:Number}, log_price_eu::Vector{T}, log_price_world::Vector{T},
-                             data::CleanData, params::Parameters) where {T <: Real}
+function constraint_jacobian(x::Vector{T}, log_price_eu::Vector{T}, log_price_world::Vector{T},
+                             data::CleanData, params::Parameters) where {T <: Number}
 
     n = data.constants.number_of_industries
 
@@ -35,8 +37,20 @@ function constraint_jacobian(x::Vector{<:Number}, log_price_eu::Vector{T}, log_p
 end
 
 
-function constraint_function(x::Vector{T}, price_eu::Vector{T}, price_world::Vector{T},
+"""
+    function constraint_function(x::Vector{T}, price_eu::Vector{T}, price_world::Vector{T},
                              params::Parameters, data::IndustryData, constants::Constants, y::Vector{T}) where {T <: Real}
+
+# Arguments
+- `x::Vector{<:Number}`: Vector containing equilibrium variables
+- `log_price_eu::Vector{<:Number}`: Vector containing the EU prices
+- `log_price_world::Vector{<:Number}`: Vector containing the rest of the world prices
+- `data::CleanData`: Cleaned data structure containing industry and regional information.
+- `params::Parameters`: Parameters structure containing production and constants.
+- `y::Vector{<:Number}` : Vector constraints will be written to. Required due to Ipopt interface
+"""
+function constraint_function(x::Vector{T}, price_eu::Vector{T}, price_world::Vector{T},
+                             params::Parameters, data::IndustryData, constants::Constants, y::Vector{T}) where {T <: Number}
 
     F = market_clearing_price_constraint(x, price_eu, price_world, params, data, constants)
     CFC = compute_fixed_capital_consumption_constraint(x, data, params)
