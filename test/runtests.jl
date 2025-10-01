6de23fbdcf006224cf119557dd0669a2fedf1d18
Arindam Saha <arindamsaha1507@gmail.com>
2025-10-01 11:19:55 +0100

    End to End test 'magically' works

--- test/runtests.jl
+++ test/runtests.jl
@@ -28,6 +28,5 @@ if(isdir(joinpath(@__DIR__,"..",settings["files"]["input_dir"])))
     include("test_minimisation.jl")
     include("test_optimisation_helpers.jl")
 
-    # TODO: End to end test fails when run through runtests.jl, but not when included by itself. Investigate
-    # include("test_end_to_end.jl")
+    include("test_end_to_end.jl")
 end
