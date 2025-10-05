241015cf54ffa92c869a4b5ab22a760b0578e1eb
Arindam Saha <arindamsaha1507@gmail.com>
2025-10-05 02:30:51 +0100

    Added test for batch estimation

--- test/test_estimation.jl
+++ test/test_estimation.jl
@@ -8,3 +8,14 @@ Random.seed!(1235)
     @test !isnothing(results)
 
 end
+
+
+@testset "Batch Estimation" begin
+
+    Supergrassi.batch_estimation(batch_size=10, log_errors=true, log_errors_filepath="test_log_errors.csv", log_results=true, log_results_filepath="test_log_results.csv")
+
+    @test isfile("test_log_results.csv")
+
+    rm("test_log_results.csv")
+
+end
